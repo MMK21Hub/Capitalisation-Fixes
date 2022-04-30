bd8904be09587598caf635512543f96cbcf42ea0
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2022-04-30 14:05:45 +0100

    Move Fix types into Fix.ts

--- src/Fix.ts
+++ src/Fix.ts
@@ -1,5 +1,24 @@
+import {
+  MinecraftLanguage,
+  MinecraftVersionSpecifier,
+} from "./minecraftHelpers.js"
 import { MultiTransformer } from "./transformers/MultiTransformer.js"
-import { FixOptions, FixData } from "./builder.js"
+import { Transformer } from "./builder.js"
+
+export interface FixOptions {
+  /** The translation string that needs to be edited */
+  key: string
+  /** A "transformer" that declares the edits that need to be made to the specified translation string. Providing an array of transformers automatically uses a {@link MultiTransformer} */
+  transformer: Transformer | Transformer[]
+  /** Specifies the versions of Minecraft that the fix should be applied to (defaults to all versions) */
+  versions?: MinecraftVersionSpecifier
+  /** Specifies the languages that the fix should be applied to (defaults to all languages) */
+  languages?: MinecraftLanguage[]
+}
+
+export interface FixData extends FixOptions {
+  transformer: Transformer
+}
 
 export class Fix {
   data
--- src/builder.ts
+++ src/builder.ts
@@ -46,21 +46,6 @@ export type LanguageFileBundle = Record<
   Record<string, LanguageFileData>
 >
 
-export interface FixOptions {
-  /** The translation string that needs to be edited */
-  key: string
-  /** A "transformer" that declares the edits that need to be made to the specified translation string. Providing an array of transformers automatically uses a {@link MultiTransformer} */
-  transformer: Transformer | Transformer[]
-  /** Specifies the versions of Minecraft that the fix should be applied to (defaults to all versions) */
-  versions?: MinecraftVersionSpecifier
-  /** Specifies the languages that the fix should be applied to (defaults to all languages) */
-  languages?: MinecraftLanguage[]
-}
-
-export interface FixData extends FixOptions {
-  transformer: Transformer
-}
-
 interface BuildOptions {
   targetVersions: MinecraftVersionSpecifier
   targetLanguages: MinecraftLanguage[]
