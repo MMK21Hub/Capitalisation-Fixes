daf24ed0c1cb2f4e7c93baa306075d9fcd14e2a4
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2022-05-02 10:15:55 +0100

    Add a fix for MC-236606

--- src/fixes.ts
+++ src/fixes.ts
@@ -1,5 +1,13 @@
 import Fix from "./Fix.js"
-import { ReplaceTransformer } from "./transformers/index.js"
+import {
+  CapitaliseFromTranslationStringsTransformer,
+  CapitaliseSegmentTransformer,
+  ReplaceTransformer,
+} from "./transformers/index.js"
+
+const autoCapitaliser = new CapitaliseFromTranslationStringsTransformer({
+  vanillaStrings: ["block.**", "item.**", "entity.**"],
+})
 
 const fixes: Fix[] = [
   new Fix({
@@ -7,6 +15,11 @@ const fixes: Fix[] = [
     key: "advancements.adventure.avoid_vibration.description",
     // Adds a serial comma to the list of blocks/mobs that can detect player vibrations
     transformer: new ReplaceTransformer(" or", ", or"),
+  }),
+  new Fix({
+    bug: "MC-236606",
+    key: "options.hideLightningFlashes.tooltip",
+    transformer: autoCapitaliser,
   }),
 ]
 
