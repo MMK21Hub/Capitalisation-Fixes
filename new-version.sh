91d8c126b8f7ad83db322e937d2893933420307e
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2022-06-01 11:44:37 +0100

    More improvements to new-version.sh

--- new-version.sh
+++ new-version.sh
@@ -7,7 +7,8 @@ fi
 
 # Generate a Resource Pack ZIP file that can be distributed with the release
 yarn build && QUIET=1 node dist/main.js $1
-echo "Built Resource Pack ZIPs: $PWD/out"
+ZIP_COUNT=$(ls -1q out/Capitalisation-Fixes-$1-*.zip | wc -l)
+echo "Built $ZIP_COUNT Resource Pack .zip file(s): $PWD/out"
 
 # Push all outstanding commits
 git push
@@ -17,3 +18,4 @@ git tag $1
 git push origin $1
 
 echo "Created new tag $1 successfully. Now go create a GitHub release!"
+echo "https://github.com/MMK21Hub/Capitalisation-Fixes/releases/new?tag=$1"
