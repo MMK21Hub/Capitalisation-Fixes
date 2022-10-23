84c0b5c79958618af9dc8b3d9a3cf8ff35edb2c8
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2022-10-23 13:04:17 +0100

    Load env variables

--- src/publisher/main.ts
+++ src/publisher/main.ts
@@ -1,4 +1,12 @@
 import ModrinthClient from "./ModrinthClient.js"
+import dotenv from "dotenv"
 
-const client = new ModrinthClient()
+// Load environment variables from the .env file
+dotenv.config()
+
+const client = new ModrinthClient({
+  baseURL: process.env.MODRINTH_API || "https://staging-api.modrinth.com",
+  token: process.env.MODRINTH_API,
+})
+
 console.log(client.createURL("hello", "there").toString())
