3fb1093e205b38ec09279d3313dbcd1b68b10cf4
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2022-10-23 13:24:08 +0100

    Start the request() function

--- src/publisher/ModrinthClient.ts
+++ src/publisher/ModrinthClient.ts
@@ -1,7 +1,14 @@
+export type URLSearchParamsResolvable = URLSearchParams | Record<string, string>
+
 export interface ModrinthClientOptions {
   token?: string
   baseURL?: string | URL
   version?: "v2" | string
+}
+
+export interface RequestOptions {
+  path: string[]
+  params: URLSearchParamsResolvable
 }
 
 export default class {
@@ -10,7 +17,21 @@ export default class {
   apiVersion
 
   createURL(...sections: string[]): URL {
+    sections = sections.map((section) => encodeURIComponent(section))
     return new URL(this.baseURL.href + [this.apiVersion, ...sections].join("/"))
+  }
+
+  async request(options: string[] | RequestOptions) {
+    const resolveOptions = (param: typeof options) =>
+      Array.isArray(param)
+        ? {
+            path: param,
+          }
+        : param
+
+    const { path } = resolveOptions(options)
+
+    const url = this.createURL(...path)
   }
 
   constructor(options: ModrinthClientOptions = {}) {
