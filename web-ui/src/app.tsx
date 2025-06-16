0ad2eb8ed4ba739a42432b9483465f003442809f
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-06-16 18:42:48 +0100

    Start the web UI using the Vite Preact template

---
+++ web-ui/src/app.tsx
@@ -0,0 +1,33 @@
+import { useState } from "preact/hooks"
+import "./app.css"
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + Preact</h1>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/app.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p>
+        Check out{" "}
+        <a
+          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
+          target="_blank"
+        >
+          create-preact
+        </a>
+        , the official Preact + Vite starter
+      </p>
+      <p class="read-the-docs">
+        Click on the Vite and Preact logos to learn more
+      </p>
+    </>
+  )
+}
