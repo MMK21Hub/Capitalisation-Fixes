36d04ee63c3aefa5e7edb6d33993246314d3f6b9
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-06-17 14:06:03 +0100

    Split the build tool into main and cli.ts
    Start working on making the build tool isomorphic

---
+++ build-tool/src/cli.ts
@@ -0,0 +1,91 @@
+import { emitResourcePacks, generateStats } from "./builder.js"
+import { MinecraftVersionRange } from "./classes/minecraftVersions.js"
+import fixes from "./fixes.js"
+import {
+  MinecraftVersionBranch,
+  MinecraftVersionSpecifier,
+} from "./helpers/minecraftHelpers.js"
+import { debugReport } from "./main.js"
+
+export async function buildPack() {
+  return await emitResourcePacks(fixes, {
+    targetVersions,
+    targetLanguages,
+    clearDirectory: true,
+    packVersion: commandLineArg,
+    // If no version was specified, just name the zip after the MC version it targets:
+    filename: commandLineArg
+      ? undefined
+      : (minecraftVersion) => `${minecraftVersion}.zip`,
+  })
+}
+
+export async function printStats(limitToLatest?: MinecraftVersionBranch) {
+  const versions: MinecraftVersionSpecifier = limitToLatest
+    ? {
+        type: "latest",
+        branch: limitToLatest,
+      }
+    : targetVersions
+
+  const stats = await generateStats(fixes, {
+    version: versions,
+  })
+
+  const { bugReports, translationKeys } = stats.count
+
+  const versionString = limitToLatest
+    ? `the latest ${limitToLatest}`
+    : `all versions`
+
+  console.log(`Statistics for ${versionString}:`)
+  console.log(`  Fixed bugs: ${bugReports}`)
+  console.log(`  Translation keys: ${translationKeys}`)
+}
+
+function getStatsFilter(): MinecraftVersionBranch | undefined {
+  if (process.argv[3] === "--latest-snapshot") return "snapshot"
+  if (process.argv[3] === "--latest-release") return "release"
+  return undefined
+}
+
+const commandLineArg = process.argv[2]
+
+const targetLanguages = ["en_us", "en_gb"]
+const targetVersions = new MinecraftVersionRange({
+  include: [
+    // Versions to build for published releases
+    // are the latest minor release for each major update or game drop
+    // new MinecraftVersionRange({ only: "1.19.4" }),
+    // new MinecraftVersionRange({ only: "1.20.2" }),
+    // new MinecraftVersionRange({ only: "1.20.6" }),
+    // new MinecraftVersionRange({ only: "1.21.1" }),
+    // new MinecraftVersionRange({ only: "1.21.3" }),
+    // new MinecraftVersionRange({ only: "1.21.4" }),
+    // new MinecraftVersionRange({ only: "1.21.5" }),
+    new MinecraftVersionRange({ only: "1.21.6-pre1" }),
+  ],
+})
+
+export async function runCLI() {
+  try {
+    commandLineArg === "--stats"
+      ? await printStats(getStatsFilter())
+      : await buildPack()
+  } catch (error) {
+    console.error(error)
+    debugger
+  } finally {
+    // Save the debug report
+    const now = new Date()
+    const date = now.toISOString().slice(0, 10)
+    const time = now.toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    })
+    debugReport.end()
+    await debugReport.exportToFile("debug", `${date} ${time}.json`)
+  }
+}
--- build-tool/src/main.ts
+++ build-tool/src/main.ts
@@ -1,101 +1,16 @@
 import { DebugReport } from "./classes/DebugReport.js"
-import { emitResourcePacks, generateStats } from "./builder.js"
-import fixes from "./fixes.js"
-import {
-  MinecraftVersionBranch,
-  MinecraftVersionSpecifier,
-  fetchVersionsSummary,
-} from "./helpers/minecraftHelpers.js"
-import { MinecraftVersionRange } from "./classes/minecraftVersions.js"
-
-export async function printStats(limitToLatest?: MinecraftVersionBranch) {
-  const versions: MinecraftVersionSpecifier = limitToLatest
-    ? {
-        type: "latest",
-        branch: limitToLatest,
-      }
-    : targetVersions
-
-  const stats = await generateStats(fixes, {
-    version: versions,
-  })
-
-  const { bugReports, translationKeys } = stats.count
-
-  const versionString = limitToLatest
-    ? `the latest ${limitToLatest}`
-    : `all versions`
-
-  console.log(`Statistics for ${versionString}:`)
-  console.log(`  Fixed bugs: ${bugReports}`)
-  console.log(`  Translation keys: ${translationKeys}`)
-}
-
-export async function buildPack() {
-  return await emitResourcePacks(fixes, {
-    targetVersions,
-    targetLanguages,
-    clearDirectory: true,
-    packVersion: commandLineArg,
-    // If no version was specified, just name the zip after the MC version it targets:
-    filename: commandLineArg
-      ? undefined
-      : (minecraftVersion) => `${minecraftVersion}.zip`,
-  })
-}
-
-function getStatsFilter(): MinecraftVersionBranch | undefined {
-  if (process.argv[3] === "--latest-snapshot") return "snapshot"
-  if (process.argv[3] === "--latest-release") return "release"
-  return undefined
-}
+import { runCLI } from "./cli.js"
+import { fetchVersionsSummary } from "./helpers/minecraftHelpers.js"
 
 export const packDescription =
   "Fixes issues with text labels.\nSource: §9§nbit.ly/CapsFix"
 
 export const debugReport = new DebugReport()
-
+export const cache = new Map<string, any>()
 console.log("Fetching Minecraft version information...")
 export const versionsSummary = await fetchVersionsSummary()
 
-export const cache = new Map<string, any>()
-
-const commandLineArg = process.argv[2]
-
-const targetVersions = new MinecraftVersionRange({
-  include: [
-    // Versions to build for published releases
-    // are the latest minor release for each major update or game drop
-    // new MinecraftVersionRange({ only: "1.19.4" }),
-    // new MinecraftVersionRange({ only: "1.20.2" }),
-    // new MinecraftVersionRange({ only: "1.20.6" }),
-    // new MinecraftVersionRange({ only: "1.21.1" }),
-    // new MinecraftVersionRange({ only: "1.21.3" }),
-    // new MinecraftVersionRange({ only: "1.21.4" }),
-    // new MinecraftVersionRange({ only: "1.21.5" }),
-    new MinecraftVersionRange({ only: "1.21.6-pre1" }),
-  ],
-})
-
-const targetLanguages = ["en_us", "en_gb"]
-
-try {
-  commandLineArg === "--stats"
-    ? await printStats(getStatsFilter())
-    : await buildPack()
-} catch (error) {
-  console.error(error)
-  debugger
-} finally {
-  // Save the debug report
-  const now = new Date()
-  const date = now.toISOString().slice(0, 10)
-  const time = now.toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false,
-  })
-  debugReport.end()
-  await debugReport.exportToFile("debug", `${date} ${time}.json`)
+if (typeof process !== "undefined") {
+  // We're being executed as a Node.js script, so run the CLI function
+  await runCLI()
 }
