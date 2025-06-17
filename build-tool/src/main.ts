36ad4740b5c3f480089c9e33c5d7f75891f0ff68
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-06-17 18:25:17 +0100

    Build tool: Export some stuff from main.ts

--- build-tool/src/main.ts
+++ build-tool/src/main.ts
@@ -4,3 +4,11 @@ if (isNode()) {
   const { runCLI } = await import("./cli.js")
   await runCLI()
 }
+
+// Export the things that are most useful for building packs in the browser
+export {
+  generateResourcePacks,
+  generateStats,
+  validateFixes,
+} from "./builder.js"
+export { MinecraftVersionRange } from "./classes/minecraftVersions.js"
