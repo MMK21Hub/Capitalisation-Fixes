832a9466287d433d325795cc21ab912b787a5dcb
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-08-30 14:09:29 +0100

    Add some version constants

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite"
+import preact from "@preact/preset-vite"
+import selfPackageJSON from "./package.json" with { type: "json" }
+import buildToolPackageJSON from "capitalisation-fixes/package.json" with {
+  type: "json"
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  define: {
+    __WEB_UI_VERSION__: JSON.stringify(selfPackageJSON.version),
+    __CAPITALISATION_FIXES_VERSION__: JSON.stringify(
+      buildToolPackageJSON.version
+    ),
+  },
 })
