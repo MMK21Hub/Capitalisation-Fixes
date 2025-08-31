93dc593e73595aafddc829448514937c70f6f8f6
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-08-31 19:54:27 +0100

    Use real version values

--- web-ui/src/constants.ts
+++ web-ui/src/constants.ts
@@ -1,2 +1,4 @@
-export const capFixesVersion = "2.FIXME"
-export const webUIVersion = "0.FIXME.1"
+// @ts-ignore Vite globals specified in vite.config.ts
+export const capFixesVersion = __CAPITALISATION_FIXES_VERSION__
+// @ts-ignore
+export const webUIVersion = __WEB_UI_VERSION__
