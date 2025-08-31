40c4df49d8a9f7b78ab467a1f613c9b4abbaa1a5
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-08-31 20:07:09 +0100

    import type

--- build-tool/src/main.ts
+++ build-tool/src/main.ts
@@ -14,5 +14,5 @@ export {
 export { MinecraftVersionRange } from "./classes/minecraftVersions.js"
 export { PACK_DESCRIPTION } from "./constants.js"
 export type { MinecraftVersionId } from "./helpers/minecraftHelpers.js"
-import Fix from "./classes/Fix.js"
+import type Fix from "./classes/Fix.js"
 export type { Fix }
